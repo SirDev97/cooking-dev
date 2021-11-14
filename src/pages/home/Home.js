ad2b3b27c03c99bbf161d9f1a7ac79de96211a93
SirDev97 <alkin971@gmail.com>
2021-11-14 14:41:32 +0100

    add use fetch hook, display data in home page from json server

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,21 @@
+import { useFetch } from '../../hooks/useFetch';
+
+// styles
 import './Home.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="error">Loading...</p>}
+      {recipes &&
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
